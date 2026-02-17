ef5e7c5775b0f2c999016df2e050acc3c2173cb3
Rodrigo Takehara <49849179+rtakehara@users.noreply.github.com>
2026-02-17 02:46:24 -0300

    Delete .config/waybar directory

--- .config/waybar/scripts/wf-recorder-status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Check if wf-recorder is running
-if pgrep -x wf-recorder >/dev/null; then
-    echo '{ "text": "Recording screen", "class": "recording","alt": "recording"}'
-else
-    echo '{ "text": "Idle", "class": "idle","alt": "idle"}'
-fi
